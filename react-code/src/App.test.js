de66f6476d62e70b6b8944e9b3029f1b5242bbfa
Andrew Davis <andrew4d3@gmail.com>
2022-11-04 08:09:53 -0300

    lecture 14 done

--- react-code/src/App.test.js
+++ react-code/src/App.test.js
@@ -1,20 +1,15 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import { logRoles } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container); // Explain this method
 
   const colorButton = screen.getByRole('button', { name: "Change to blue" });
 
-  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
 });
-
-/*
-  What are we doing here?
-  1. Rendering the app
-  2. In the screen, getting the element which role is "button"
-  3. Expecting that element we retrive in the last step to have a background color of red
-*/
 
 test("button turns blue when clicked", () => {
 
