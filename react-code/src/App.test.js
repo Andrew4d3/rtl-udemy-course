f354e4779be9092a1a93614f96cdeda6ebcec271
Andrew Davis <andrew4d3@gmail.com>
2022-11-04 08:29:23 -0300

    Lecture 15 done

--- react-code/src/App.test.js
+++ react-code/src/App.test.js
@@ -1,17 +1,27 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import { logRoles } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial color", () => {
-  const { container } = render(<App />);
-  logRoles(container); // Explain this method
+test("button has correct initial color and updates when clicked", () => {
+  render(<App />);
 
   const colorButton = screen.getByRole('button', { name: "Change to blue" });
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton).toHaveTextContent('Change to red');
 });
 
-test("button turns blue when clicked", () => {
+/* 
+What are we doing here?
 
-})
+1. Rendering the app
+2. Getting element to be tested using getByRole
+3. Asserting that element has correct backgroundColor
+4. Clicking element
+5. Asserting that element has changed its color to "blue"
+
+*/
 
