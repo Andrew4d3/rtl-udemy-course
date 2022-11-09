4ec00df9b8e6b8c08502ddc245ae4dc955ca06a3
Andrew Davis <andrew4d3@gmail.com>
2022-11-09 08:21:14 -0300

    Lecture 34 done

--- react-code/src/App.test.js
+++ react-code/src/App.test.js
@@ -4,38 +4,38 @@ import App from "./App";
 test("button has correct initial color and updates when clicked", () => {
   render(<App />);
 
-  const colorButton = screen.getByRole('button', { name: "Change to blue" });
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
   fireEvent.click(colorButton);
 
-  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
-  expect(colorButton).toHaveTextContent('Change to red');
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveTextContent("Change to red");
 });
 
 test("initial conditions", () => {
   render(<App />);
 
-  const colorButton = screen.getByRole('button', { name: "Change to blue"});
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toBeEnabled();
 
-  const checkbox = screen.getByRole('checkbox');
+  const checkbox = screen.getByRole("checkbox");
   expect(checkbox).not.toBeChecked();
 });
 
 test("clicked conditions", () => {
   render(<App />);
-  const colorButton = screen.getByRole('button');
-  const checkbox = screen.getByRole('checkbox', { name: "Disable button"});
+  const colorButton = screen.getByRole("button");
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
 
   fireEvent.click(checkbox);
-  expect(colorButton).toHaveStyle({ backgroundColor: 'gray' })  
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
   expect(colorButton).toBeDisabled();
 
   fireEvent.click(checkbox);
   expect(colorButton).toBeEnabled();
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
-})
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
 
-/* What do we do here? */
+/* What do we do */
