b89faf6d4029ba3b9fec17d2ecb58b78af6b81c3
Andrew Davis <andrew4d3@gmail.com>
2022-11-15 08:25:17 -0300

    Lecture 44 done

--- react-code/src/pages/summary/test/SummaryForm.test.jsx
+++ react-code/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("SummaryForm component", () => {
@@ -11,19 +12,41 @@ describe("SummaryForm component", () => {
     expect(button).toBeDisabled();
   });
 
-  it("should enable/disable button when checkbox is checked/unchecked", () => {
+  it("should enable/disable button when checkbox is checked/unchecked", async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
     const checkbox = screen.getByRole("checkbox");
     const button = screen.getByRole("button", { name: "Confirm order" });
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     expect(button).toBeEnabled();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     expect(button).toBeDisabled();
   });
-});
 
-/* Explain what these two tests are doing */
+  test("popover responds to hover", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
+  });
+
+  /* What this code does? */
+});
