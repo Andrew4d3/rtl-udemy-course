1bdf680eace2de2b4addec1ff15ea4abbcae02bf
Andrew Davis <andrew4d3@gmail.com>
2022-11-23 08:14:46 -0300

    Lecture 53 done

--- react-code/src/pages/entry/tests/Options.test.jsx
+++ react-code/src/pages/entry/tests/Options.test.jsx
@@ -2,14 +2,15 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("displays image for each scoop option from server", () => {
+test("displays image for each scoop option from server", async () => {
+  //
   render(<Options optionType="scoops" />);
 
-  const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
-  expect(scoopImages).toHaveLenght(2);
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
 
   const altText = scoopImages.map((element) => element.alt);
-  expect(altText).toEqual("Chocale scoop", "Vanilla scoop");
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
 
 /* Explain what we did there */
