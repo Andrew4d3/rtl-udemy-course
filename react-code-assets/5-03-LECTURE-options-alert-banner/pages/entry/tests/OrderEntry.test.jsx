8d392f2b1418db136558aee2c393a30c9da87077
Andrew Davis <andrew4d3@gmail.com>
2022-11-25 08:26:50 -0300

    Lecture 59 done

--- react-code-assets/5-03-LECTURE-options-alert-banner/pages/entry/tests/OrderEntry.test.jsx
+++ react-code-assets/5-03-LECTURE-options-alert-banner/pages/entry/tests/OrderEntry.test.jsx
@@ -3,7 +3,7 @@ import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
 
-test("handles error for scoops and toppings routes", async () => {
+test.only("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
--- react-code/src/pages/entry/Options.jsx
+++ react-code/src/pages/entry/Options.jsx
@@ -3,19 +3,24 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    // @ts-ignore
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
