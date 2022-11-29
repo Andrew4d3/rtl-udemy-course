fca0e3c75759bd4192b32243c0b39dfc349f5826
Andrew Davis <andrew4d3@gmail.com>
2022-11-29 08:40:33 -0300

    Lecture 67 done

--- react-code/src/pages/entry/tests/totalUpdates.test.jsx
+++ react-code/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,13 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Options from "../Options";
+import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 
 test("update scoop subtotal when scoops change", async () => {
   const user = userEvent.setup();
-  render(<Options optionType="scoops" />);
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider }); // Explain this
 
   const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
-  expect(scoopsSubtotal).toHaveTextContext("0.00");
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
 
   const vanillaInput = await screen.findByRole("spinbutton", {
     name: "Vanilla",
@@ -15,7 +16,7 @@ test("update scoop subtotal when scoops change", async () => {
   await user.clear(vanillaInput);
   await user.type(vanillaInput, "1");
 
-  expect(scoopsSubtotal).toHaveTextContext("2.00");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
 
   const chocolateInput = await screen.findByRole("spinbutton", {
     name: "Chocolate",
@@ -23,5 +24,5 @@ test("update scoop subtotal when scoops change", async () => {
 
   await user.clear(chocolateInput);
   await user.type(chocolateInput, "2");
-  expect(scoopsSubtotal).toHaveTextContext("6.00");
+  expect(scoopsSubtotal).toHaveTextContent("24.00");
 });
