e9f86a854f46cb040c8c62d868f48b3023847c63
Andrew Davis <andrew4d3@gmail.com>
2022-11-29 08:29:31 -0300

    Lecture 68 done

---
+++ react-code/src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("update scoop subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContext("0.00");
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  expect(scoopsSubtotal).toHaveTextContext("2.00");
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContext("6.00");
+});
