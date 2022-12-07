e2e835bf34e19dc1696af2a779325fa171d92652
Andrew Davis <andrew4d3@gmail.com>
2022-12-07 08:12:22 -0300

    Lecture 81 done

--- react-code/src/mocks/handlers.js
+++ react-code/src/mocks/handlers.js
@@ -19,4 +19,8 @@ export const handlers = [
       ])
     );
   }),
+
+  rest.post("http://localhost:3030/order", (req, res, ctx) => {
+    return res(ctx.json({ orderNumber: 123455676 }));
+  }),
 ];
